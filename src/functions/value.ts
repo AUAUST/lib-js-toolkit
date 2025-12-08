45f848915ea77dc98541ced08bac8be5eec06e80
apademide <contact@apademide.ch>
2025-12-08 13:08:11 +0100

    imp: `value()` calls with `this`

--- src/functions/value.ts
+++ src/functions/value.ts
@@ -1,10 +1,14 @@
-export type Value<V> = V | (() => V);
+export type Value<V, T = any> = V | ((this: T) => V);
 
 export function value<V>(subject: Value<V>): V;
-export function value<V, A extends any[]>(
-  subject: V | ((...args: A) => V),
+export function value<V, A extends any[], T>(
+  subject: V | ((this: T, ...args: A) => V),
   ...args: A
 ): V;
-export function value(subject: unknown, ...args: unknown[]): unknown {
-  return typeof subject === "function" ? subject(...args) : subject;
+export function value(
+  this: any,
+  subject: unknown,
+  ...args: unknown[]
+): unknown {
+  return typeof subject === "function" ? subject.call(this, ...args) : subject;
 }
