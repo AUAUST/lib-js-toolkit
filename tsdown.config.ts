b99b3e8efcbf43b1e705a8f907702400fcf12f3f
apademide <63295420+apademide@users.noreply.github.com>
2026-09-25 12:37:55 +0200

    meta: prevent CI to succeed with outdated package.json

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,11 @@
+import { readFile } from "node:fs/promises";
 import { defineConfig } from "tsdown";
 import { getTsdownOptions } from "./codegen/index.js";
+import initialPackageJson from "./package.json" with { type: "json" };
 
-export default defineConfig({
+const packageJsonUrl = new URL("./package.json", import.meta.url);
+
+export default defineConfig((_, { ci }) => ({
   ...getTsdownOptions(),
   dts: true,
   exports: {
@@ -17,4 +21,20 @@ export default defineConfig({
     ignoreRules: ["cjs-resolves-to-esm"],
   },
   deps: { onlyBundle: [] },
-});
+  failOnWarn: ci,
+  hooks: {
+    "build:done"() {
+      if (ci) {
+        return readFile(packageJsonUrl, "utf8").then((content) => {
+          if (
+            JSON.stringify(initialPackageJson) !==
+            JSON.stringify(JSON.parse(content))
+          ) {
+            throw new Error("package.json is not up to date");
+          }
+        });
+      }
+    },
+  },
+  minify: ci,
+}));
